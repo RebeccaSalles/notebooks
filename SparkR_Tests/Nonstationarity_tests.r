faa851140a58325a7c065528810707524e5820e0
Rebecca Pontes Salles <rebeccapsalles@gmail.com>
2017-12-07 23:12:53 -0200

    Update: Aprendizado_de_Maquina Comp_Paralela_Distribuida SparkR_Tests

--- SparkR_Tests/Nonstationarity_tests.r
+++ SparkR_Tests/Nonstationarity_tests.r
@@ -5,23 +5,83 @@
 
 # COMMAND ----------
 
+# MAGIC %md ####Parallel and Distributed Computing - Masters Degree Course on ComputerScience - CEFET/RJ
+# MAGIC __Professor:__ Eduardo Ogasawara; __Student:__ Rebecca Pontes Salles; __December, 08 of 2017__
+
+# COMMAND ----------
+
 # MAGIC %md ####1 - Introduction
 
 # COMMAND ----------
 
-# MAGIC %md Data preprocessing is a key step for mining and learning from data, and one of its main tasks is the transformation of data. This task is very important in the context of time series analysis, since most of the time series methods and models assume the property of stationarity, i.e., statistical properties do not change over time, which in practice is the exception and not the rule in most real datasets. There are several transformation methods designed to handle nonstationarity in time series, amomg them the moving average smoother (MAS). 
+# MAGIC %md Data preprocessing is a key step for mining and learning from data, and one of its main tasks is the transformation of data. This task is very important in the context of time series analysis, since most of the time series methods and models assume the property of stationarity, i.e., statistical properties do not change over time, which in practice is the exception and not the rule in most real datasets. There are several transformation methods designed to handle nonstationarity in time series, amomg them the moving average smoother (__MAS__). 
 # MAGIC 
-# MAGIC MAS has been widely used especially in finance and econometrics, being useful for highlighting seasonality and long-term trends and patterns in a time series. MAS can detect the evolving behavior of a time series by minimizing random noise, and it can also be used for seasonal adjustment of a time series (Shumway andStoffer 2010; Ogasawaraet al.2010).
+# MAGIC __MAS__ has been widely used especially in finance and econometrics, being useful for highlighting seasonality and long-term trends and patterns in a time series. MAS can detect the evolving behavior of a time series by minimizing random noise, and it can also be used for seasonal adjustment of a time series (Shumway and Stoffer 2010; Ogasawara et al.2010).
 # MAGIC 
-# MAGIC This work presents an experiment for applying an optimization function that derives "best" parameters of MAS for each time series in a dataset. Since the computation performed by this function is The focus of this work is to generate practical knowledge regarding their advantages and limitations to the problem of time series prediction. A subset of the reviewed transformation methods are compared by means of an experimental evaluation in \proglang{R} using the Package \pkg{TSPred}, benchmark datasets from time series prediction competitions and other real macroeconomic datasets from Brazil.
+# MAGIC This work presents an experiment for applying an optimization function that derives "best" parameters of __MAS__ for each time series in a dataset. This function, named __fittestMAS__, was implemented and will soon be published in a new version (4.0.0) of the R-package __TSPred__. Since this function presents a somewhat high computational cost, especially when applied to large datasets, this work presents the distribution of its computation using a cluster (in __Databricks__) and __Spark__. The __SparkR__ is also used as a frontend for Spark. For the experiments, __fittestMAS__ is applied to benchmark datasets from time series prediction competitions. All datasets used are available in the __TSPred__ package.
 
 # COMMAND ----------
 
-# MAGIC %md ####1 - Installing and Loading Packages
+# MAGIC %md ####2 - The fittestMAS function
 
 # COMMAND ----------
 
-# MAGIC %md #####Installing and Loading Packages on the Driver
+# MAGIC %md The __fittestMAS__ function implements the __MAS__ method, and optimizes its main parameter (order of moving average) according to the best prediction results on a cross-validation dataset. In this case, the best prediction results are defined by the smaller produced value of MSE (mean squared error) prediction accuracy measure. The general idea implemented by the function is:
+# MAGIC 1. if no prior knowledge of the moving average order is given, it generates order options to be evaluated;
+# MAGIC 2. it runs __MAS__ of each order on the provided time series and performs predictions with each of these transformed series;
+# MAGIC 3. generates MSE errors for the predictions using validation data;
+# MAGIC 4. selects the moving average order which generated the smaller MSE error as optim;
+# MAGIC 5. using the selected optim order, generates predictions and evaluates the prediction error of the original time series using a test dataset.
+
+# COMMAND ----------
+
+# MAGIC %md ####3 - Spark and SparkR
+
+# COMMAND ----------
+
+# MAGIC %md __Apache Spark__ is an engine for distributed data processing, having a data-sharing abstraction called “Resilient Distributed Datasets” (RDDs). RDDs are a collection of data distributed over several nodes of a cluster that can be referenced as a single (local) collection. It can capture a wide range of processing workloads that previously needed separate engines, including SQL, streaming, machine learning, and graph processing (Zaharia et al.). Spark's applications are easier to develop using a unified API and it can run diverse functions over the same data, often in memory. One of the main libraries of Spark is the __SparkSQL__, which provides a higher-level abstraction called __DataFrames__, which conceptually are RDDs full of records (tuples) with a known schema (Zaharia et al.).
+# MAGIC 
+# MAGIC The Spark architecture follows the following image:
+# MAGIC 
+# MAGIC ![Spark architecture][logo]
+# MAGIC [logo]: http://blog.cloudera.com/wp-content/uploads/2015/09/pyspark.png "Spark architecture"
+# MAGIC 
+# MAGIC * The Driver node: creates a the SparkContext and RDDs and stages ip or sends off transformation and action functions.
+# MAGIC * The Cluster Manager: allocates resources
+# MAGIC 
+# MAGIC Recently it was developed, __SparkR__ an R package that provides a frontend to Apache Spark and uses Spark’s distributed computation engine to enable large scale data analysis from the R shell (Venkataraman et al.). It addresses a limitation of __R__, which is usually limited by single threaded computation and that can only process data sets that fit in a single machine’s memory. It uses the same structures as __SparkSQL__, that is the __DataFrames__, and also has general SQL functions in its API. Starting with Spark 1.4.x, SparkR provides a distributed DataFrame implementation that supports operations like selection, filtering, aggregation etc. (similar to R data frames, dplyr) but on large datasets.
+# MAGIC 
+# MAGIC Since the version 2.0 of __SparkR__, it presents functions like dapply, gapply and spark.lapply that allow the use of user defined functions (UDF) to be applied to DataFrames.  To this experiment this functions will be the most appropriate.
+
+# COMMAND ----------
+
+# MAGIC %md ####4 - Spark in the Databricks Cluster
+
+# COMMAND ----------
+
+# MAGIC %md Databricks is a unified analytics platform that is optimized to the use of Spark, which is natively installed and configured. Furthermore, with the release of Spark 1.4.0, SparkR was inlined in Apache Spark. At that time Databricks released R Notebooks to be the first company that officially supports SparkR. To further facilitate usage of SparkR, Databricks R notebooks imported SparkR by default and provided a working sqlContext object.
+# MAGIC 
+# MAGIC Due to the beneficial environment provided by Databricks it was selected for this experiment. Databricks makes it ease to create, configure and use a cluster. In particular Databricks runs on Amazon Web Services (AWS) for cloud infrastructure. This fact provides the possibilities of increasing productivity and collaboration, build on a secure, trusted cloud and scale without limits. AWS clusters and infrastructure management and data science are simplified with a fully managed and secure platform powered by Databricks Runtime.
+# MAGIC 
+# MAGIC In order to begin our experiments using Databricks the following steps were addressed:
+# MAGIC 1. an account in Databricks was created;
+# MAGIC 2. an account in AWS was created;
+# MAGIC 3. information of the AWS account was configured in Databricks.
+# MAGIC 
+# MAGIC Occurred problems:
+# MAGIC * __Regarding step 1__: The free account option of Databricks (Databricks Community) 
+
+# COMMAND ----------
+
+# MAGIC %md ####5 - Experiments
+
+# COMMAND ----------
+
+# MAGIC %md #####5.1 - Installing and Loading Packages
+
+# COMMAND ----------
+
+# MAGIC %md ######Installing and Loading Packages on the Driver
 
 # COMMAND ----------
 
@@ -31,28 +91,25 @@ library("TSPred")
 
 # COMMAND ----------
 
-# MAGIC %md #####Installing and Loading Packages on Workers
+# MAGIC %md ######Installing and Loading Packages on Workers
 
 # COMMAND ----------
 
 numWorkers <- 2
 spark.lapply(1:numWorkers, function(x) {
   #Pacotes necessarios para funcoes em fittest_models_functions
-  install.packages(c("TSPred","KFAS","car","forecast","wavelets","EMD","vars"), repos = "http://cran.us.r-project.org")
-  #Pacotes necessarios para funcoes em stats_properties_functions
-  install.packages(c("urca","tseries","stats","lmtest","car","nortest","plyr"), repos = "http://cran.us.r-project.org")
+  install.packages(c("TSPred","forecast"), repos = "http://cran.us.r-project.org")
   #Pacotes necessarios para funcoes em results_analysis_functions
-  install.packages(c("TSPred","KFAS","MuMIn","openair","ggplot2","corrplot","devtools","Cairo","plyr"), repos = "http://cran.us.r-project.org")
-  devtools::install_github("vsimko/corrplot")
+  install.packages(c("ggplot2","Cairo"), repos = "http://cran.us.r-project.org")
 })
 
 # COMMAND ----------
 
-# MAGIC %md ####2 - First tests
+# MAGIC %md #####5.2 - First Tests with SparkR Functions
 
 # COMMAND ----------
 
-# MAGIC %md #####No parallelization
+# MAGIC %md ######No Parallelization Test
 
 # COMMAND ----------
 
@@ -72,7 +129,7 @@ fPolyR
 
 # COMMAND ----------
 
-# MAGIC %md #####How not to do it: spark.lapply
+# MAGIC %md ######How Not to Do It: Parallelization with spark.lapply
 
 # COMMAND ----------
 
@@ -93,7 +150,7 @@ print(execTime)
 
 # COMMAND ----------
 
-# MAGIC %md #####How not to do it 2: gapplyCollect
+# MAGIC %md ######How Not to Do It (The Sequel): Parallelization with gapplyCollect
 
 # COMMAND ----------
 
@@ -134,7 +191,7 @@ fPolyR
 
 # COMMAND ----------
 
-# MAGIC %md #####Now we're talking: dapplyCollect
+# MAGIC %md ######Now we're talking: Parallelization with dapplyCollect
 
 # COMMAND ----------
 
@@ -175,11 +232,11 @@ fPolyR
 
 # COMMAND ----------
 
-# MAGIC %md ####3 - Evaluating Nonstationary Time Series Methods
+# MAGIC %md #####5.3 - Evaluating fittestMAS with and without Parallelization
 
 # COMMAND ----------
 
-# MAGIC %md #####Loading experiment datasets and functions
+# MAGIC %md ######Loading experiment datasets and functions
 
 # COMMAND ----------
 
@@ -195,7 +252,7 @@ source_data("https://github.com/RebeccaSalles/TSPred/blob/master/dev/12.17/exp/E
 # COMMAND ----------
 
 #Analysis of fittness and prediction of many series and many transforms
-sparkr.MASExp <- function(timeseries,timeseries.test, numWorkers, numWorkerCores,
+sparkr.MASExp <- function(timeseries,timeseries.test, numProc,
                           rank.by=c("MSE","NMSE","MAPE","sMAPE","MaxError","AIC","AICc","BIC","logLik")){
     
   n.ahead <- nrow(timeseries.test)
@@ -205,7 +262,7 @@ sparkr.MASExp <- function(timeseries,timeseries.test, numWorkers, numWorkerCores
   
   dataset.df <- createDataFrame(dataset)
   
-  rdataset.df <- repartition(dataset.df, numPartitions = numWorkerCores*numWorkers)
+  rdataset.df <- repartition(dataset.df, numPartitions = numProc)
   
   partitionExp <- function(timeseries){
     lenTS <- ncol(timeseries)
@@ -266,14 +323,47 @@ MASExp(CATS,CATS.cont, rank.by="MSE")
 
 # COMMAND ----------
 
-times <- list()
+times <- NULL
 for(i in 1:10){
-  times[[i]] <- MASExp(CATS,CATS.cont, rank.by="MSE")$time
+  # Clear all cached tables in the current session
+  clearCache()
+  t <- MASExp(CATS,CATS.cont, rank.by="MSE")$time
+  times <- rbind(times,data.frame(user=t[1],system=t[2],elapsed=t[3]))
 }
 
 # COMMAND ----------
 
-sparkr.times <- list()
+par(mfrow = c(1, 2))
+boxplot(times[,c("elapsed")], width=10, height=4)
+boxplot(sparkr.times[,c("elapsed")], width=10, height=4)
+#boxplot(times[,c("system")], width=10, height=4)
+#boxplot(times[,c("elapsed")], width=10, height=4)
+
+# COMMAND ----------
+
+numWorkers<-2
+numWorkerCores<-2
+sparkr.times <- NULL
 for(i in 1:10){
-  times[[i]] <- sparkr.MASExp(CATS,CATS.cont, numWorkers=2, numWorkerCores=2, rank.by="MSE")$time
+  t <- sparkr.MASExp(CATS,CATS.cont, numProc=numWorkers*numWorkerCores, rank.by="MSE")$time
+  sparkr.times <- rbind(sparkr.times,data.frame(user=t[1],system=t[2],elapsed=t[3]))
+}
+
+# COMMAND ----------
+
+par(mfrow = c(1, 3))
+boxplot(times[,c("user")], width=10, height=4)
+boxplot(times[,c("system")], width=10, height=4)
+boxplot(times[,c("elapsed")], width=10, height=4)
+
+# COMMAND ----------
+
+numWorkers<-2
+numWorkerCores<-2
+sparkr.times.proc <- NULL
+for(numProc in 1:(numWorkers*numWorkerCores)){
+  for(i in 1:10){
+    t <- sparkr.MASExp(CATS,CATS.cont, numProc=numProc, rank.by="MSE")$time
+    sparkr.times.proc <- rbind(sparkr.times.proc,data.frame(numProc=numProc,user=t[1],system=t[2],elapsed=t[3]))
+  }
 }
